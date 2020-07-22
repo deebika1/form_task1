17c8f0fac6e19a809f062cbe7e32986de00ab879
dpka <deebikakishore22@gmail.com>
2020-07-22 09:32:03 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
 import React from "react";
 
 const App=()=> {
+  const[Array,setArray]=React.useState([]);
   const [first, setFirst] = React.useState([]);
   const [last, setLast] = React.useState([]);
   const [email, setEmail] = React.useState([]);
   const [password, setPassword] = React.useState([]);
   const [repassword, setRepassword] = React.useState([]);
- function display(){
-   console.log(first,last,email,password,repassword);
-//alert(first,last,email,password,repassword);
+  const[marr,setMarr]=React.useState([]);
+  const[country,setCountry]=React.useState([]);
+  const[food,setFood]=React.useState([]);
+  const[state,setState]=React.useState([]);
+  const[select,setSelect]=React.useState([]);
+  // const selection = [
+  //   {
+  //     name: "INDIA",
+  //     states: ["tamilnadu", "kerala", "Delhi"],
+  //   },
+  //   {
+  //     name: "USA",
+  //     states: ["New York", "California", "Texas"],
+  //   },
+  // ];
+  // // function random_function(e) {
+     
+  // //   setCountry(e.target.value);
+  // //   setState(selection.filter((value, i) => value.name == e.target.value)[0].states);
+    
+  // // }
+  
+  function handleEnter(event) {
+    if (event.keyCode === 13) {
+      const form = event.target.form;
+      const index = Array.prototype.indexOf.call(form, event.target);
+      form.elements[index + 1].focus();
+      event.preventDefault();
+    }
   }
+ function display(){
+   if(password==repassword){
+              return(
+            
+   setArray(Array.concat([{first,last,email,password,marr,food,country},]),
+
+   
+//alert(first,last,email,password,repassword);
+  setFirst(""),
+  setLast(""),
+  setEmail(""),
+  setPassword(""),
+  setRepassword(""),
+  setMarr(""),
+  setFood(""),
+  setCountry("")
+   )            
+              )}
+              else 
+              {
+                alert("incorrect password")
+              }
+ }
+
+  
   return (
     <div>
       <div
@@ -63,20 +115,20 @@ const App=()=> {
         <label>Matrial status</label>
 
         <form action="/action_page.php">
-          <input type="radio" />
-          <label value="single">Single</label>
+          <input type="radio"  name="status" value="single" onChange={(e)=>setMarr(e.target.value)} />
+          <label >Single</label>
           <br></br>
-          <input type="radio" />
-          <label value="married">Married</label>
+          <input type="radio" name="status" value="married" onChange={(e)=>setMarr(e.target.value)}/>
+          <label >Married</label>
           <br></br>
-          <input type="radio" />
-          <label value="cant say">Can't say</label>
+          <input type="radio" name="status" value="cant say" onChange={(e)=>setMarr(e.target.value)}/>
+          <label >Can't say</label>
         </form>
         <br></br>
 
         <form action="/action_page.php">
           <label> choose your food</label>
-          <select name="foods" id="foods">
+          <select name="foods" id="foods" onChange={(e)=>setFood(e.target.value)}>
             <option value="Briyani">Briyani</option>
             <option value="Thandoori">Thandoori</option>
             <option value="Icecream">IceCream</option>
@@ -86,14 +138,22 @@ const App=()=> {
         <br></br>
         <form action="/action_page.php">
           <label> choose your country</label>
-          <select name="country" id="country">
+          <select name="country" id="country" onChange={(e)=>setCountry(e.target.value)}>
             <option value="India">India</option>
             <option value="America">America</option>
           </select>
            </form>
           <br></br>
+            <br />
+            <label>Select state:</label>
+            <select onChange={(e) => setSelect(e.target.value)} onKeyDown={handleEnter}>
+              <option>--Choose State--</option>
+              {state.map((data, i) => {
+                return <option key={i}>{data}</option>;
+              })}
+            </select>
          <button onClick={()=>display()}>submit</button>
-        
+         
       </div>
       <div
         style={{
@@ -109,13 +169,24 @@ const App=()=> {
               <th>First Name</th>
               <th>Second Name</th>
               <th>Email</th>
-              <th>{last}</th>
+              <th>password</th>
+              <th>Marital status</th>
+              <th> choice of your food</th>
+              <th> enter your country</th>
+              <th>select your state</th>
             </tr>
           </thead>
           <tbody>
-            {first.map((val, index) => (
+            {Array.map((val, index) => (
               <tr key={index}>
-                <td>{val}</td>
+                <td>{val.first}</td>
+                <td>{val.last}</td>
+                <td>{val.email}</td>
+                <td>{val.password}</td>
+                <td>{val.marr}</td>
+                <td>{val.food}</td>
+                <td>{val.country}</td>
+                <td>{val.state}</td>
               </tr>
             ))}
           </tbody>
